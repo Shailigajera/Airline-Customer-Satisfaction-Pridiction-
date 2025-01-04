9565fdaca0cec33d23ad9a67a9284ff341e23729
shaili gajera <shailigajera@gmail.com>
2025-01-04 10:38:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup , find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+setup(
+    name ="mlops",
+    version="0.1",
+    author="shaili",
+    author_emails="abc@gmail.com",
+    description="MLOPS PROJECT",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+)
